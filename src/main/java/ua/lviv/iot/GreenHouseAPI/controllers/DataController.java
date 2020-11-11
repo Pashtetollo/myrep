2f6fc8dfc1943c8668c622faec6f143f1dcada1e
Roman Spodaryk <roman.spodaryk@ukr.net>
2020-11-11 13:25:09 +0200

    modified DataController

--- src/main/java/ua/lviv/iot/GreenHouseAPI/controllers/DataController.java
+++ src/main/java/ua/lviv/iot/GreenHouseAPI/controllers/DataController.java
@@ -1,10 +1,14 @@
 package ua.lviv.iot.GreenHouseAPI.controllers;
 
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.lviv.iot.GreenHouseAPI.models.Data;
 import ua.lviv.iot.GreenHouseAPI.services.DataService;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -14,28 +18,39 @@ public class DataController {
     @Autowired
     DataService dataService;
 
+    private HashMap<String, String> responseMap;
+
     @GetMapping
-    public List<Data> getAllData() {
-        return dataService.getAllData();
+    public ResponseEntity<List<Data>> getAllData() {
+        return new ResponseEntity<>(dataService.getAllData(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Data getDataById(@PathVariable int id) {
-        return dataService.getDataById(id);
+    public ResponseEntity<Data> getDataById(@PathVariable int id) {
+        return new ResponseEntity<>(dataService.getDataById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public void addNewData(@RequestBody Data data) {
+    public ResponseEntity<JSONObject> addNewData(@RequestBody Data data) {
         dataService.addData(data);
+        responseMap = new HashMap<>();
+        responseMap.put("message", "Successfully created");
+        return new ResponseEntity<>(new JSONObject(responseMap), HttpStatus.CREATED);
     }
 
     @DeleteMapping
-    public void deleteAllData() {
+    public ResponseEntity<JSONObject> deleteAllData() {
+        responseMap = new HashMap<>();
+        responseMap.put("message", "Successfully deleted all objects");
         dataService.deleteALLData();
+        return new ResponseEntity<>(new JSONObject(responseMap), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAllData(@PathVariable int id) {
+    public ResponseEntity<JSONObject> deleteAllData(@PathVariable int id) {
+        responseMap = new HashMap<>();
+        responseMap.put("message", "Successfully deleted");
         dataService.deleteDataById(id);
+        return new ResponseEntity<>(new JSONObject(responseMap), HttpStatus.OK);
     }
 }
